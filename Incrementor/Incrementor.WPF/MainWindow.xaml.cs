f7e4b7c64e7649f911a011331c4ee39d46652918
Andrija Eremic <andrija.eremic@gmail.com>
2025-01-11 19:38:06 +0100

    upgrade to net8 and ui changes

--- Incrementor/Incrementor.WPF/MainWindow.xaml.cs
+++ Incrementor/Incrementor.WPF/MainWindow.xaml.cs
@@ -1,28 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace Incrementor.WPF
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
-	}
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string inputFilePath = string.Empty;
+        private string outputFilePath = string.Empty;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InputLocationButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new()
+            {
+                DefaultExt = ".xlsx",
+                Filter = "Excel Files (*.xlsx)|*.xlsx",
+            };
+
+            var result = dlg.ShowDialog();
+
+            if (result != true)
+            {
+                return;
+            }
+
+            inputFilePath = dlg.FileName;
+            InputLocationTextBox.Text = inputFilePath;
+        }
+
+        private void SaveLocationButton_Click(object sender, RoutedEventArgs e)
+        {
+            var folderDialog = new OpenFolderDialog();
+
+            if (folderDialog.ShowDialog() != true)
+            {
+                return;
+            }
+
+            outputFilePath = folderDialog.FolderName;
+            SaveLocationTextBox.Text = outputFilePath;
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(inputFilePath)
+                || string.IsNullOrEmpty(outputFilePath))
+            {
+                return;
+            }
+            
+            var incrementorParsingResult = Logic.Incrementor
+                .ProcessData(inputFilePath, Path.Combine(outputFilePath,"Output.xlsx"));
+            if (incrementorParsingResult.ParsingResult)
+            {
+                MessageBox.Show(
+                    $"New file saved at {incrementorParsingResult.OutputFilePath}",
+                    "Success",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show(
+                    $"New file not saved." +
+                    $" Code: {(int)incrementorParsingResult.ErrorType}. " +
+                    $"{incrementorParsingResult.ErrorMessage}",
+                    "Error",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+        }
+    }
 }
